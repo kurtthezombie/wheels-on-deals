86f997378c8da8b5736a81015ed45342e849b875
kurtthezombie <kurt.desmond619@gmail.com>
2023-12-29 12:35:40 +0800

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WheelsOnDeals.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(USER_INFO model)
+        {
+            /*/insert something idk
+            _context.USER_INFO.Add(model);
+            _context.SaveChanges();
+            ViewBag.Message = "Data inserted successfully.";*/
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ViewModels/SignUpViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+using WheelsOnDeals.Models;
+
+namespace WheelsOnDeals.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public USER_INFO User { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required.")]
+        [Compare("User.USER_PASSWORD", ErrorMessage = "Passwords do not match.")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
