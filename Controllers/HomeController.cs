4f11bdc781f8ee218065fe33b5401228db941fd3
kurtthezombie <kurt.desmond619@gmail.com>
2023-12-30 20:13:10 +0800

    Login and Sign up page with validation.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,18 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WheelsOnDeals.Models;
 
 namespace WheelsOnDeals.Controllers
 {
     public class HomeController : Controller
     {
+        CAR_SELLING_Entities db = new CAR_SELLING_Entities();
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(USER_INFO user)
+        {
+            var checkLogin = db.USER_INFO.Where(x => x.USER_EMAIL.Equals(user.USER_EMAIL) 
+            && x.USER_PASSWORD.Equals(user.USER_PASSWORD)).FirstOrDefault();
+            
+            if (checkLogin != null)
+            {
+                Session["Session_UserId"] = user.USER_IDN.ToString();
+                Session["Session_UserEmail"] = user.USER_EMAIL.ToString();
+                Session["Session_UserFirstName"] = checkLogin.USER_FNAME.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "Incorrect username and password";
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index","Home");
         }
 
         [HttpGet]
@@ -23,13 +54,33 @@ namespace WheelsOnDeals.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Signup(USER_INFO model)
+        public ActionResult Signup(USER_INFO user)
         {
-            /*/insert something idk
-            _context.USER_INFO.Add(model);
-            _context.SaveChanges();
-            ViewBag.Message = "Data inserted successfully.";*/
-            return RedirectToAction("Index");
+            if(db.USER_INFO.Any(x => x.USER_EMAIL == user.USER_EMAIL))
+            {
+                ViewBag.Notification = "Email is already in use.";
+                return View();
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    db.USER_INFO.Add(user);
+                    db.SaveChanges();
+
+                    
+                    return RedirectToAction("Index", "Home");
+                } 
+                else
+                {
+                    return View(user);
+                }
+            }   
+        }
+
+        public ActionResult ListUsers()
+        {
+            return View(db.USER_INFO.ToList());
         }
 
         public ActionResult Contact()
